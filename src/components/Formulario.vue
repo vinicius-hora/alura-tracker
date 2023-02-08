f3cf83285eb7512aead3edfd17c2ed6f3e40d89c
vinicius-hora <vinicius_hora@live.com>
2023-02-08 08:32:23 -0300

    first commit

---
+++ src/components/Formulario.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="box formulario">
+    <div class="columns">
+      <div
+        class="column is-8"
+        role="form"
+        aria-label="Formulário para criação de uma nova tarefa"
+      >
+        <input
+          type="text"
+          class="input"
+          placeholder="Qual tarefa você deseja iniciar?"
+          v-model="descricao"
+        />
+      </div>
+      <div class="column">
+        <Temporizador @aoTemporizadorFinalizado="finalizarTarefa" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Temporizador from "./Temporizador.vue";
+
+export default defineComponent({
+  name: "Formulario",
+  emits: ['aoSalvarTarefa'],
+  components: {
+    Temporizador,
+  },
+  data(){
+    return{
+        descricao: ''
+    }
+  },
+  methods: {
+    finalizarTarefa(tempoDecorrido: number): void {
+      console.log("tempo da tarefa", tempoDecorrido);
+      console.log("descricao", this.descricao);
+      this.$emit('aoSalvarTarefa',{
+        duracaoEmSegundos: tempoDecorrido,
+        descricao: this.descricao
+      })
+
+      this.descricao = ''
+    },
+  },
+});
+</script>
+
+<style>
+.formulario{
+    color: var(--texto-primario);
+    background-color: var(--bg-primario);
+}
+</style>
